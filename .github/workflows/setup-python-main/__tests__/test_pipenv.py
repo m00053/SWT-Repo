d387223c5233c702c9c3cc575c56bebcd22972d1
F. Scheel <fscheel@users.noreply.github.com>
2023-12-16 22:38:44 +0100

    pipenv.py changed

--- .github/workflows/setup-python-main/__tests__/test_pipenv.py
+++ .github/workflows/setup-python-main/__tests__/test_pipenv.py
@@ -1,9 +1,13 @@
-import numpy as np
+#import numpy as np
 
 def fun_a():
-    a = np.array([2, 3, 4])
-    print(type(a))
+#    a = np.array([2, 3, 4])
+    a = [2, 3, 4]
+#    print(type(a))
+    print(a)
 
 def fun_b():
-    b = np.array([1.2, 3.5, 5.1])
-    print(type(b))
+#    b = np.array([1.2, 3.5, 5.1])
+    b = [1.2, 3.5, 5.1]
+#    print(type(b))
+    print(b)
