1b63918357949882b0b573ac0f060099f3080327
F. Scheel <fscheel@users.noreply.github.com>
2023-12-16 23:21:53 +0100

    cleancode.py & dirtycode.py deleted

--- .github/workflows/setup-python-main/__tests__/test_pipenv.py
+++ .github/workflows/setup-python-main/__tests__/test_pipenv.py
@@ -1,12 +1,15 @@
+"""Module providing numerical functions"""
 #import numpy as np
 
 def fun_a():
+    """Function printing a"""
 #    a = np.array([2, 3, 4])
     a = [2, 3, 4]
 #    print(type(a))
     print(a)
 
 def fun_b():
+    """Function printing b"""
 #    b = np.array([1.2, 3.5, 5.1])
     b = [1.2, 3.5, 5.1]
 #    print(type(b))
--- .github/workflows/setup-python-main/__tests__/verify_python.py
+++ .github/workflows/setup-python-main/__tests__/verify_python.py
@@ -1,33 +1,25 @@
+"""Module providing system functions"""
 import sys
-argCount = len(sys.argv) - 1
+arg_count = len(sys.argv) - 1
 
-def print_test():
-    if argCount == 1:
-        expectedVersion = sys.argv[1]
-        versions = len(expectedVersion.split("."))
-        MAJOR_MINOR = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
+"""Function verifying python"""
+def verify_python():
+	if arg_count == 1:
+		expected_version = sys.argv[1]
+		versions = len(expected_version.split("."))
+		major_minor = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
 
-        if versions == 2:
-            # Test only major and minor version
-            if expectedVersion != MAJOR_MINOR:
-                raise Exception("")
-                #    logging.exception(e)
-                #raise Exception("Incorrect MAJOR_MINOR v.: " + MAJOR_MINOR + "\nExpected: " + expectedVersion)
-                #raise Exception("Incorrect major + minor version detected\nExpected: " + expectedVersion + "\nActual: " + MAJOR_MINOR)
-        elif versions == 3:
-            # Test major, minor and micro version
-            MAJOR_MINOR_MICRO = MAJOR_MINOR + '.' + str(sys.version_info[2])
-            if expectedVersion != MAJOR_MINOR_MICRO:
-                raise Exception("")
-                #    logging.exception(e)
-                #raise Exception("Incorrect MAJOR_MINOR micro v.: " + MAJOR_MINOR_MICRO + "\nExpected: " + expectedVersion)
-                #raise Exception("Incorrect major + minor + micro version detected\nExpected: " + expectedVersion + "\nActual: " + MAJOR_MINOR_MICRO)
-        else:
-            raise Exception("")
-            #    logging.exception(e)
-            #raise Exception("Incorrect number of arguments supplied")
-        print("Correct version of Python " + expectedVersion + " detected")
-    else:
-        raise Exception("")
-        #    logging.exception(e)
-        #raise Exception("Incorrect number of arguments supplied")
+		if versions == 2:
+			# Test only major and minor version
+			if expected_version != major_minor:
+				raise NameError("")
+		elif versions == 3:
+			# Test major, minor and micro version
+			major_minor_micro = major_minor + '.' + str(sys.version_info[2])
+			if expected_version != major_minor_micro:
+				raise NameError("")
+			raise NameError("")
+		else: 
+		print("Correct version of Python " + expected_version + " detected")
+	else:
+		raise NameError("")
